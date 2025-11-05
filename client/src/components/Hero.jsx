777abf7aa2c953d60021c9bdcd658d816d10719d
AlexONagy <143730322+AlexONagy@users.noreply.github.com>
2025-11-05 13:51:55 -0500

    Fixed css
    there was some color and squishing issues with css, these are now fixed

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../App.css';
 
-
 export default function Hero() {
     return (
     <section className="hero">
         <div className="hero-content">
             <h1>Monmouth University Cybersecurity Research Center</h1>
             <p>Join us in advancing cybersecurity education, innovation, and collaboration.</p>
-            <button onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}>Learn More</button>
+            <button onClick={() => document.getElementById('upload').scrollIntoView({ behavior: 'smooth' })}>Upload Research</button>
         </div>
     </section>
     );
