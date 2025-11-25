0fb91ba507d14c727d840e4b5e810988131e4523
Mary Cotrupi <cotrum@rpi.edu>
2025-11-25 02:46:37 -0500

    events adjust

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -6,8 +6,14 @@ const Events = () => {
   return (
     <div>
       <Header />
-      <h1>Events</h1>
-      <p>Upcoming cybersecurity events and workshops.</p>
+      <div className="page-container">
+        <h1 className="page-title">Events Calendar</h1>
+        <iframe
+        src="https://outlook.office365.com/owa/calendar/911c64dd96e54c01aa6208421868645d@rpi.edu/051a8861feda43d0871df85155911a5015038586886625751096/calendar.html"
+        style={{ border: 0, width: "100%", height: "1000px" }}
+        title="Outlook Calendar"
+      ></iframe>
+      </div>
       <Footer />
     </div>
   );
