ea508e4ff08011a4d3a52f3de8c1c13547f210da
Mary Cotrupi <cotrum@rpi.edu>
2025-11-26 00:59:12 -0500

    Update Events.jsx

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -9,7 +9,7 @@ const Events = () => {
       <div className="page-container">
         <h1 className="page-title">Events Calendar</h1>
         <iframe
-        src="https://outlook.office365.com/owa/calendar/911c64dd96e54c01aa6208421868645d@rpi.edu/051a8861feda43d0871df85155911a5015038586886625751096/calendar.html"
+        src="https://outlook.office365.com/owa/calendar/911c64dd96e54c01aa6208421868645d@rpi.edu/9967a24c3b4c4bb39842bc96654be9b35403998818310970382/calendar.html"
         style={{ border: 0, width: "100%", height: "1000px" }}
         title="Outlook Calendar"
       ></iframe>
